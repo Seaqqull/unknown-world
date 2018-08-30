af4a3fa6ece8b273e5a1a013fdf89e5074915bad
Dima <seaqqullprogramming@gmail.com>
2018-08-30 16:57:58 +0300

    Fixed camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnknownWorld
+{
+    public class CameraController : MonoBehaviour
+    {
+        public enum InputChoice
+        {
+            KeyboardAndMouse, Controller,
+        }
+
+        [System.Serializable]
+        public struct InvertSettings
+        {
+            public bool invertX;
+            public bool invertY;
+        }
+
+        public Transform m_targetPosition;
+        public Transform m_targetlookAtPosition;
+        public InputChoice inputChoice;
+        public InvertSettings keyboardAndMouseInvertSettings;
+        public InvertSettings controllerInvertSettings;
+
+        public bool allowRuntimeCameraSettingsChanges;
+        public float m_DampTime = 0.2f;
+
+
+        private bool m_isCameraActive = false;
+        private Camera m_Camera;
+        private float m_zoomVelocity;
+        private Vector3 m_moveVelocity;
+
+        private void Awake()
+        {
+            m_Camera = GetComponentInChildren<Camera>();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+            if(m_isCameraActive)
+                Move();         
+        }
+
+        private void Move()
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, m_targetPosition.position, ref m_moveVelocity, m_DampTime);
+        }
+
+        private void Zoom()
+        {
+            /*Some actions to perform zooming on objects*/
+            //m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_zoomVelocity, m_DampTime);
+        }
+
+        public void ActivateCamera(Transform cameraTarget, Transform cameraLookAt) {
+            m_isCameraActive = true;
+
+            m_targetPosition = cameraTarget;
+            m_targetlookAtPosition = cameraLookAt;
+        }
+
+        public void DiactivateCamera() {
+            m_isCameraActive = false;
+
+            m_targetPosition = null;
+            m_targetlookAtPosition = null;
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnknownWorld
+{
+    [System.Serializable]
+    public class CameraKeeperData {
+        public Transform m_target;
+        public Transform m_lookAt;
+    }
+
+
+    public class GameManager : MonoBehaviour
+    {
+
+        public CameraController[] m_camerasInLevel; // at [0] default camera
+        public CameraKeeperData[] m_cameraTagets; // at [0] always must be the player
+
+        private int? m_activeCamera = null;
+        public int m_targetCamera = -1;
+
+        // Use this for initialization
+        void Start()
+        {
+            SetCameraTargetFromPlayer();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (m_activeCamera.HasValue && m_targetCamera != m_activeCamera)
+                SetAcctiveCamera();
+        }
+
+
+        private void SetCameraTargetFromPlayer()
+        {
+            if(m_targetCamera != -1)
+                m_camerasInLevel[m_targetCamera].ActivateCamera(m_cameraTagets[m_targetCamera].m_target, m_cameraTagets[m_targetCamera].m_lookAt);
+            m_activeCamera = m_targetCamera == -1 ? 0 : m_targetCamera;
+        }
+
+        private void SetAcctiveCamera(int cameraId = -1) {
+            if (cameraId == -1)
+                cameraId = m_targetCamera;
+            m_camerasInLevel[m_activeCamera ?? 0].ActivateCamera(m_cameraTagets[m_activeCamera ?? 0].m_target, m_cameraTagets[m_activeCamera ?? 0].m_lookAt);
+            m_camerasInLevel[cameraId].ActivateCamera(m_cameraTagets[cameraId].m_target, m_cameraTagets[cameraId].m_lookAt);
+        }
+
+    }
+}
