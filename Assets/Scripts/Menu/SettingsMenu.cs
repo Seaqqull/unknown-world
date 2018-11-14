a34e1f8f2486afb01fe64c0ea134d3fad63bb6cf
Dima <seaqqullprogramming@gmail.com>
2018-11-14 18:16:59 +0200

    Update  Add:  - Start \ Settings menu

---
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using System.Linq;
+using System;
+
+public class SettingsMenu : MonoBehaviour {
+
+    [SerializeField] private AudioMixer m_audioMixer;
+    [SerializeField] private Dropdown m_graphicsDropdown;
+
+    private Resolution[] m_resolutions;
+
+    private void Start()
+    {
+        m_resolutions = Screen.resolutions;
+        m_graphicsDropdown.ClearOptions();
+
+        List<string> options = Array.ConvertAll(m_resolutions, x => x.width + "x" + x.height).ToList();
+        m_graphicsDropdown.AddOptions(options);
+        m_graphicsDropdown.value = 
+            options.FindIndex((res) => res == Screen.currentResolution.width + " x " + Screen.currentResolution.height);
+        m_graphicsDropdown.RefreshShownValue();
+    }
+
+    public void SetVolume(float volume)
+    {
+        m_audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetGraphicQuality(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Screen.SetResolution(m_resolutions[resolutionIndex].width, m_resolutions[resolutionIndex].height, Screen.fullScreen);
+    }
+}
