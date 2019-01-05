f7d07a2588069ebf662114785629c98e7cac0d56
Dima <seaqqullprogramming@gmail.com>
2019-01-05 21:46:46 +0200

    Update  Add:  - Advanced audio setting

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -6,10 +6,35 @@ using UnityEngine.UI;
 using System.Linq;
 using System;
 
+[System.Serializable]
 public class SettingsMenu : MonoBehaviour {
 
+    [System.Serializable]
+    public class AudioSetting
+    {        
+        [SerializeField] private string m_exposedParam;        
+        [SerializeField] private Slider m_slider;
+
+        public string ExposedParamn
+        {
+            get { return this.m_exposedParam; }
+        }
+        public Slider Slider
+        {
+            get { return this.m_slider; }
+        }
+
+
+        public void Initialize()
+        {
+            m_slider.value = PlayerPrefs.GetFloat(m_exposedParam);
+        }
+    }
+
+
+    [SerializeField] private AudioSetting[] m_audioSettings;
     [SerializeField] private Dropdown m_graphicsDropdown;
-    [SerializeField] private AudioMixer m_audioMixer;    
+    [SerializeField] private AudioMixer m_audioMixer;
 
     private Resolution[] m_resolutions;
 
@@ -24,12 +49,45 @@ public class SettingsMenu : MonoBehaviour {
         m_graphicsDropdown.value = 
             options.FindIndex((res) => res == Screen.currentResolution.width + " x " + Screen.currentResolution.height);
         m_graphicsDropdown.RefreshShownValue();
+
+        for (int i = 0; i < m_audioSettings.Length; i++)
+        {
+            m_audioSettings[i].Initialize();
+        }
     }
 
 
+    private float GetExposedValue(string audioName)
+    {
+        for (int i = 0; i < m_audioSettings.Length; i++)
+        {
+            if (m_audioSettings[i].ExposedParamn == audioName)
+            {
+                return m_audioSettings[i].Slider.value;
+            }
+        }
+        return 0.0f;
+    }
+
+
+    public void SetMusic(float volume)
+    {
+        m_audioMixer.SetFloat("Main", GetExposedValue("Main"));
+    }
+
     public void SetVolume(float volume)
     {
-        m_audioMixer.SetFloat("Volume", volume);
+        m_audioMixer.SetFloat("Volume", GetExposedValue("Volume"));
+    }
+
+    public void SetEffects(float volume)
+    {
+        m_audioMixer.SetFloat("Effects", GetExposedValue("Effects"));
+    }
+
+    public void SetInterface(float volume)
+    {
+        m_audioMixer.SetFloat("Interface", GetExposedValue("Interface"));
     }
 
     public void SetGraphicQuality(int index)
